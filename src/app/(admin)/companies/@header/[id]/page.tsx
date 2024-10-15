910ee3eda32db4edd330302b4362adea626b045e
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-10-15 17:17:30 +0300

    start

--- src/app/(admin)/companies/@header/[id]/page.tsx
+++ src/app/(admin)/companies/@header/[id]/page.tsx
@@ -3,18 +3,20 @@ import { Company, getCompany } from '@/app/lib/api';
 import getQueryClient from '@/app/lib/utils/getQueryClient';
 import React from 'react';
 
-
 export interface PageProps {
   params: { id: string };
 }
 
 export default async function Page({ params }: PageProps) {
   const queryClient = getQueryClient();
+
   await queryClient.prefetchQuery({
     queryKey: ['companies', params.id],
     queryFn: () => getCompany(params.id, { cache: 'no-store' }),
     staleTime: 10 * 1000,
   });
+
   const company = queryClient.getQueryData(['companies', params.id]) as Company;
+
   return <Header>{company?.title}</Header>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 // import { redirect } from 'next/navigation';
+import React from 'react';
 
 export default function Home() {
   // redirect('/dashboard');
