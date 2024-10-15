9d0c284b80ee8d1053f9d3bef64a714bdcfcfda1
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-10-15 15:29:13 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('/dashboard');
-  return null;
+  // redirect('/dashboard');
+  // return null;
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
