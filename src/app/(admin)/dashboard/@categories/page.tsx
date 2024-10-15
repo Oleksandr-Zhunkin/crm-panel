a5f51e915376d3f8056da5d6f12b3718841f2b9f
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-10-16 01:01:56 +0300

    Update page.tsx

--- src/app/(admin)/dashboard/@categories/page.tsx
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -3,10 +3,23 @@ import StatCard, { StatCardType } from '@/app/app/components/StatCard';
 import { getCategories, getCompanies } from '@/app/lib/api';
 import getCountById from '@/app/lib/utils/getCountById';
 import React from 'react';
+import mongoose from 'mongoose';
+
+const { ObjectId } = mongoose.Types;
 
 export default async function Page() {
   const categories = await getCategories();
   const companies = await getCompanies();
+  categories.forEach((category) => {
+    if (!ObjectId.isValid(category.id)) {
+      throw new Error(`Invalid ObjectId for category: ${category.id}`);
+    }
+  });
+  companies.forEach((company) => {
+    if (!ObjectId.isValid(company.id)) {
+      throw new Error(`Invalid ObjectId for company: ${company.id}`);
+    }
+  });
   const counts = getCountById(companies, 'categoryId');
 
   return (
